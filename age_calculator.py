09df019af070bdb8fca469de513b2b3b298b0ac4
alanoudalshalani <107769859+alanoudalshalani@users.noreply.github.com>
2022-10-05 10:38:15 +0300

    just now

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,26 @@
+from calendar import month
 from datetime import date
 
 
 def get_dob():
-    # write code here
-	...
+	y = input("Enter year of birth:")
+	m = input("Enter month of birth:")
+	d = input("Enter day of birth:")
+	return date(int(y), int(m), int(d))
 
 
-def get_age(dob):
-    # write code here
-	...
+
+
+def get_age(d):
+    today = date.today()
+    diff = today - d
+    return diff.days // 365
 
 
 def main():
-	# write code here
-	...
+    d = get_dob()
+    age = get_age(d)
+    print(f"You are {age} year(s) old!")
 
 
 if __name__ == '__main__':
